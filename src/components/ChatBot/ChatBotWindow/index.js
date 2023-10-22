58fe60bbc1dabfc16e75c318353818806d14cea7
elnurnabivi <elnurnabivi@gmail.com>
2023-10-22 17:07:32 +0200

    Modified chatbot

--- src/components/ChatBot/ChatBotWindow/index.js
+++ src/components/ChatBot/ChatBotWindow/index.js
@@ -1,24 +1,48 @@
-import ChatBot from 'react-simple-chatbot';
-import ChatCloseIcon from '../Icons/ChatCloseIcon';
+import ChatBot from "react-simple-chatbot";
+import ChatCloseIcon from "../Icons/ChatCloseIcon";
 
 const steps = [
   {
-    id: '0',
-    message: 'Hey there! What is your name?',
-    trigger: '1',
-  },  {
-    id: '1',
-    user: true,
-  }
+    id: "Greet",
+    message: "Hello, welcome to our website",
+    trigger: "Ask name",
+  },
+  { id: "Ask name", message: "Please tell me your name", trigger: "Name" },
+  { id: "Name", user: true, trigger: "Ask issue" },
+  {
+    id: "Ask issue",
+    message: "Dear {previousValue}, please select your issue",
+    trigger: "issues",
+  },
+  {
+    id: "issues",
+    options: [
+      {
+        value: "Site related issue",
+        label: "Site related issue",
+        trigger: "issue",
+      },
+      { value: "Other issue", label: "Other issue", trigger: "issue" },
+    ],
+  },
+  {
+    id: "issue",
+    message: "Thank you for letting us your issue. We will contact you asap",
+    end: true,
+  },
 ];
-
-const ChatBotWindow = ({className, setShowChatBotWindow}) => {
+const ChatBotWindow = ({ className, setShowChatBotWindow }) => {
   return (
     <div className={className}>
-      <ChatCloseIcon width={25} height={25} className="chatbot__close" onClick={() => setShowChatBotWindow(false)} />
-      <ChatBot steps={steps} />
+           {" "}
+      <ChatCloseIcon
+        width={25}
+        height={25}
+        className="chatbot__close"
+        onClick={() => setShowChatBotWindow(false)}
+      />
+            <ChatBot steps={steps} />   {" "}
     </div>
-  )
-}
-
+  );
+};
 export default ChatBotWindow;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import HamburgerIcon from './HamburgerIcon';
-import CloseIcon from './CloseIcon';
-import { Link, NavLink } from 'react-router-dom';
-import Button from '../Button/index'
-import Logo from '../Icons/Logo';
-import './style.css';
+import React, { useState, useEffect } from "react";
+import HamburgerIcon from "./HamburgerIcon";
+import CloseIcon from "./CloseIcon";
+import { Link, NavLink } from "react-router-dom";
+import Button from "../Button/index";
+import Logo from "../Icons/Logo";
+import "./style.css";
 
 const Header = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
 
   const toggleNav = () => {
     window.scrollTo(0, 0);
@@ -18,35 +19,95 @@ const Header = () => {
     setIsNavExpanded(false);
   };
 
+  useEffect(() => {
+    const checkMobileView = () => {
+      setIsMobileView(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", checkMobileView);
+
+    checkMobileView();
+
+    return () => {
+      window.removeEventListener("resize", checkMobileView);
+    };
+  }, []);
+
   return (
     <>
-    <div className={`placeholder ${isNavExpanded ? 'placeholder--visible' : ''}`} />
-    <header className={`header ${isNavExpanded ? 'header--fixed' : ''}`}>
-      <nav className="header__nav global-padding">
-        <Link className="header__logo-container" to="#" onClick={handleLink}>
-          <Logo width={200} height={50}/>
-        </Link>
-        <div className="header__menu-container">
-          <button type="button" className="header__button" onClick={toggleNav} aria-label="toggle-nav" aria-expanded="false">
-            { isNavExpanded ? <CloseIcon /> : <HamburgerIcon />}
-          </button>
-          <ul className={`header__links-container ${isNavExpanded ? 'header__links-container--visible' : ''}`}>
-            <li><NavLink className="header__link" to="#" onClick={handleLink}>Startups</NavLink></li>
-            <li><NavLink className="header__link" to="#" onClick={handleLink}>Contact</NavLink></li>
-            <li><NavLink className="header__link" to="#" onClick={handleLink}>
-              <Button
-                variant={'secondary'}
-                size={'md'}
-                name={'Work with us'}
-                >
-                Work with us!
-              </Button>
-            </NavLink></li>
+      <div
+        className={`placeholder ${isNavExpanded ? "placeholder--visible" : ""}`}
+      />
+      <header className={`header ${isNavExpanded ? "header--fixed" : ""}`}>
+        <nav className="header__nav global-padding">
+          <Link className="header__logo-container" to="#" onClick={handleLink}>
+            <Logo width={200} height={50} />
+          </Link>
+          {isMobileView ? (
+            <div className="header__menu-container">
+              <button
+                type="button"
+                className="header__button"
+                onClick={toggleNav}
+                aria-label="toggle-nav"
+                aria-expanded={isNavExpanded}
+              >
+                {isNavExpanded ? <CloseIcon /> : <HamburgerIcon />}
+              </button>
+            </div>
+          ) : (
+            <ul className="header__links-container">
+              <li>
+                <NavLink className="header__link" to="#" onClick={handleLink}>
+                  Startups
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="header__link" to="#" onClick={handleLink}>
+                  Contact
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="header__link" to="#" onClick={handleLink}>
+                  <Button
+                    variant={"secondary"}
+                    size={"md"}
+                    name={"Work with us"}
+                  >
+                    Work with us!
+                  </Button>
+                </NavLink>
+              </li>
+            </ul>
+          )}
+        </nav>
+        {isMobileView && (
+          <ul
+            className={`header__links-container ${
+              isNavExpanded ? "header__links-container--visible" : ""
+            }`}
+          >
+            <li>
+              <NavLink className="header__link" to="#" onClick={handleLink}>
+                Startups
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="header__link" to="#" onClick={handleLink}>
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="header__link" to="#" onClick={handleLink}>
+                <Button variant={"secondary"} size={"md"} name={"Work with us"}>
+                  Work with us!
+                </Button>
+              </NavLink>
+            </li>
           </ul>
-        </div>
-      </nav>
-    </header>
-  </>
+        )}
+      </header>
+    </>
   );
 };
 
