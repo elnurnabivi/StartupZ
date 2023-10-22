e796e0c740f25dfcfa2bb6f251989c25b90c48ac
elnurnabivi <elnurnabivi@gmail.com>
2023-10-22 17:25:41 +0200

    Deploy setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/startupz">
     <App />
   </BrowserRouter>
   // </React.StrictMode>
 );
+
+reportWebVitals();
